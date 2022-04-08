4b0dd81f5bcfbba98487a4790c9920ef44fe4b9c
sumanth <sumanth.sarvepalli@gmail.com>
2022-04-08 11:17:12 +0530

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+ *
+ * Title        : Mini-Shell
+ * Author       : Sumanth
+ * Description  : This Project is to develop a command line interface
+ *                which mimics the actions of a shell. 
+ * Tools Used   : System Calls, Signals, IPC mechanisms
+ *
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to Mini-Shell Project\n");
+
+    return 0;
+}
